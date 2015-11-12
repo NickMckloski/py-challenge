5573277cad6c5f41a35fa28c0a6161ab71facfaa
Nick Mckloski <mckloski.nick@gmail.com>
2015-11-12 17:51:12 -0500

    fizz buzz done, readme updated

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # py-challenge
-python challenge exercises.
 
 exercises I have done in python saved here for reference in the future.
--- fizbuzz.py
+++ fizbuzz.py
@@ -6,3 +6,16 @@ current number is divisible by both 3 and 5 the function should write “FizzBuz
 '''
 
 # Solution:
+
+n = int(input("Enter a number to stop at: "))
+
+for i in range(1, n):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
