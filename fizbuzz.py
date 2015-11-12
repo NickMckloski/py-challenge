9648c1e7740cc406e7ef97e21cc04f1b364ae04a
Nick Mckloski <mckloski.nick@gmail.com>
2015-11-12 17:42:52 -0500

    fizz buzz challenge added

---
+++ fizbuzz.py
@@ -0,0 +1,8 @@
+'''
+You are to write a function that displays the numbers from 1 to an input parameter n, one per line,
+except that if the current number is divisible by 3 the function should write “Fizz” instead of the number,
+if the current number is divisible by 5 the function should write “Buzz” instead of the number, and if the
+current number is divisible by both 3 and 5 the function should write “FizzBuzz” instead of the number.
+'''
+
+# Solution:
